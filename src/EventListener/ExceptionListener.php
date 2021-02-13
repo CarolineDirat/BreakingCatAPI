e15929e6202be865ef787cb8b43f996637328c01
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2021-02-13 17:00:26 +0100

    add src/EventListener/ExceptionListener.php to custom errors in json response in production mode

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    /**
+     * environment = value of APP_ENV.
+     *
+     * @var string
+     */
+    private string $environment;
+
+    public function __construct(string $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    /**
+     * onKernelException.
+     *
+     * The response will be a json response the error occurred
+     *
+     * @param ExceptionEvent $event
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        if ('prod' === $this->environment) {
+            $exception = $event->getThrowable();
+
+            $result['content'] = 'Oops! An Error Occurred';
+            $code = $exception->getCode();
+            $code = empty($code) ? Response::HTTP_BAD_REQUEST : $code;
+            $result['error'] = [
+                'code' => $code,
+                'text' => Response::$statusTexts[$code],
+                'message' => $exception->getMessage(),
+            ];
+            $result['_links'] = [
+                'Get a random card' => [
+                    'href' => '/api/ramdom-jpeg',
+                    'method' => 'GET',
+                ],
+            ];
+
+            $response = new JsonResponse($result, $result['error']['code'], []);
+
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            $event->setResponse($response);
+        }
+    }
+}
