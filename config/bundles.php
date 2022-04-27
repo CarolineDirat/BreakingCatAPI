1efe15b2866c7417266eabf3b677acd2d61f976b
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2022-04-27 11:43:28 +0200

    webpack encore installation

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
