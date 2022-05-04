d9ab6e685bfc74feebd6e7347106ebbbc8cc271c
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2022-05-04 19:37:45 +0200

    Add spinner during loading new card (#49) (#50)
    * add spinner during loading new card (#49)
    
    * fix app.js - Codacy
    
    * improve loader displaying
    
    * move loader in front of the 'click here!' button
    
    * hide footer during loading
    
    * fix display bug when card is to heavy

--- assets/app.js
+++ assets/app.js
@@ -6,34 +6,51 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
 
 // loads the jquery package from node_modules
-import $ from 'jquery';
-import { async } from 'regenerator-runtime';
+import $ from "jquery";
+import { async } from "regenerator-runtime";
 
 // import the function from greet.js (the .js extension is optional)
 // ./ (or ../) means to look for a local file
 // import greet from './greet';
 
-$(document).ready(function() {
-    //$('body').prepend('<h1>'+greet('jill')+'</h1>');
-       
-    //////////////////// homepage - call a new card ////////////////////////
-    
-    const clickHere = $('#homepage-click-here');
-    const homepageCard = $('#homepage-card');
-    var urlNewCard = homepageCard.data('url');
+$(document).ready(function () {
+  //$('body').prepend('<h1>'+greet('jill')+'</h1>');
 
-    clickHere.on('click', function(e) {
-        $.ajax({url: urlNewCard})
-            .then(function (data) {
-                homepageCard.empty();
-                const card = '<img id="home-card" src="/homeCards/' + data.sessionId + '/'+ data.filename + '" class="img-fluid" alt="Cataas photo with Breaking Bad Quotes">';
-                homepageCard.append(card);
-            });
-    });
+  //////////////////// homepage - call a new card ////////////////////////
+
+  const spinner = $("#homepage-spinner");
+  const homepageCard = $("#homepage-card");
+  $(document).ajaxStop(function () {
+    $(".spinner-border").addClass("d-none");
+  });
+
+  const clickHere = $("#homepage-click-here");
+  var urlNewCard = homepageCard.data("url");
+
+  clickHere.on("click", function (e) {
+    spinner.removeClass("d-none");
+    $("#footer").addClass("d-none");
+    $.ajax({ url: urlNewCard })
+      .then(function (data) {
+        homepageCard.empty();
+        const card =
+          '<img id="home-card" src="/homeCards/' +
+          data.sessionId +
+          "/" +
+          data.filename +
+          '" class="img-fluid" alt="Cataas photo with Breaking Bad Quotes">';
+        homepageCard.append(card);
+      })
+      .then(function () {
+        setTimeout(() => {
+          $("#footer").removeClass("d-none");
+        }, 200);
+      });
+  });
 });
